50a2ceebd589799cd4ac235ab73374e11b7231c7
developiaa <developiaa@gmail.com>
2022-08-19 23:43:02 +0900

    feat: Add bottom navigation with icons, related package

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
 
 import IndexScreen from './src/screens/IndexScreen';
 
 const App: () => Node = () => {
   return (
-    <SafeAreaView>
+    <>
       <IndexScreen />
-    </SafeAreaView>
+    </>
   );
 };
 
